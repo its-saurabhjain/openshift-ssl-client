392ec30f54f93bce64f4de121eefea8fc6a17162
unknown <itsaurabh@yahoo.com>
2020-05-04 07:24:13 -0700

    Http2-Client demo

---
+++ README.md
@@ -0,0 +1,12 @@
+# OpenShift SSL Client
+
+To run the client run:
+```
+oc process -f https://raw.githubusercontent.com/domenicbove/openshift-ssl-client/master/template.yaml | oc create -f -
+```
+This will automatically start a build and trigger a deployment.Check the logs to see the response from the server
+
+To cleanup
+```
+oc delete all -l app=ssl-client
+```
---
+++ src/main/java/com/example/sslclient/SslClientApplication.java
@@ -0,0 +1,31 @@
+package com.example.sslclient;
+
+import java.io.IOException;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.URI;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslClientApplication {
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+	HttpClient client = new HttpClient();
+	GetMethod method = new GetMethod();
+	//String uri = "https://ssl-server." + System.getenv("POD_NAMESPACE") + ".svc:8443/secured";
+	String uri = "https://http2server." + System.getenv("POD_NAMESPACE") + ".svc:8443/secured";
+	method.setURI(new URI(uri, false));
+	while(true) {
+	    client.executeMethod(method);
+	    System.out.println("Http_Protocol");
+	    System.out.println(method.getRequestHeader("Protocol"));
+	    Thread.sleep(5000);
+	}
+
+    }
+
+}
+
